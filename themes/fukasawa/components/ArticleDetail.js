96e708d9354831a5434eb71d83dc4cf09e68d0dc
epskai <questionlearndatabase@gmail.com>
2024-05-16 18:32:08 +1000

    adjust color

--- themes/fukasawa/components/ArticleDetail.js
+++ themes/fukasawa/components/ArticleDetail.js
@@ -53,7 +53,7 @@ export default function ArticleDetail(props) {
                     {post.category}
 
                   </Link>
-                <span className='mr-2 dark:text-gray-500'>|</span>
+                <span className='mr-2 dark:text-gray-50'>|</span>
               </>)}
 
               {post?.type !== 'Page' && (<>
